ae93f9a4dbb0734fcd370502df891d049fb538f8
sercul <sercul@gmail.com>
2012-10-01 16:11:15 +0300

    registration and sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 FrProject::Application.routes.draw do
 
+  resources :users
+  resources :posts
+  resources :sessions, :only => [:new, :create, :destroy]
+
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
 
-  root :to => 'pages#home'
+  root :to => 'posts#index'
 
-
+  match '/signup',  :to => 'users#new'
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120928145720) do
+ActiveRecord::Schema.define(:version => 20120930204133) do
 
   create_table "microposts", :force => true do |t|
     t.string   "content"
@@ -20,11 +20,22 @@ ActiveRecord::Schema.define(:version => 20120928145720) do
     t.datetime "updated_at", :null => false
   end
 
+  create_table "posts", :force => true do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "create_time"
+    t.integer  "user_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "password"
+    t.string   "salt"
   end
 
 end
