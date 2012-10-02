fc21b54fb27724242ec24ca6e03a296234ad7582
sercul <sercul@gmail.com>
2012-10-02 16:34:01 +0300

    sign in out

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,10 @@
 require 'digest'
 class User < ActiveRecord::Base
-  attr_accessible :email, :name  ,:password
+  attr_accessible :email, :name, :password
   has_many :microposts
-  validates :name,:email,:password , :presence => true
+  validates :name, :email, :password, :presence => true
 
   before_save :encrypt_password
-
 
 
   def has_password?(submitted_password)
@@ -15,8 +14,14 @@ class User < ActiveRecord::Base
 
   def self.authenticate(email, submitted_password)
     user = find_by_email(email)
-    return nil  if user.nil?
+    return nil if user.nil?
     return user if user.has_password?(submitted_password)
+  end
+
+  def self.authenticate_with_salt(id, cookie_salt)
+    user = find_by_id(id)
+    return nil if user.nil?
+    return user if user.salt == cookie_salt
   end
 
   private
@@ -37,7 +42,6 @@ class User < ActiveRecord::Base
   def secure_hash(string)
     Digest::SHA2.hexdigest(string)
   end
-
 
 
 end
