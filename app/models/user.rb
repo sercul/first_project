5f0b71903dd17b4712571cb38ae5acf410c15aa7
sercul <sercul@gmail.com>
2012-10-15 15:38:47 +0300

    sign in out

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,15 @@
 require 'digest'
 class User < ActiveRecord::Base
-  attr_accessible :email, :name, :password
+  attr_accessible :email, :name, :password, :password_confirmation
   has_many :microposts
-  validates :name, :email, :password, :presence => true
+  validates :name, :email, :presence => true
+
+  validates :password,
+            :confirmation => true
+
+  validates :password, :presence => true, :on => :create,
+            :confirmation => true,
+            :length => {:within => 6..40}
 
   before_save :encrypt_password
 
@@ -24,11 +31,25 @@ class User < ActiveRecord::Base
     return user if user.salt == cookie_salt
   end
 
+  def check_hz(id)
+    user = User.find_by_id(id)
+    if user.nil?
+      return nil
+    else
+      return user.password
+    end
+  end
+
   private
 
   def encrypt_password
     self.salt = make_salt if new_record?
-    self.password = encrypt(password)
+    if (password != '')
+      self.password = encrypt(password)
+    else
+      self.password = check_hz(self.id)
+    end
+
   end
 
   def encrypt(string)
